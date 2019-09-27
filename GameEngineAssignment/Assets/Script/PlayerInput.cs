e23b6eb8433c28b72af32f3e029ffa522edb183b
Sepehr <sepehr_325@hotmail.com>
2019-09-27 15:53:30 -0400

    version 1 unity finished

---
+++ GameEngineAssignment/Assets/Script/PlayerInput.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+
+
+public class PlayerInput : MonoBehaviour
+{
+
+
+    float speed = 5;
+    Wrapper Plugin;
+    Vector3 rootpos;
+    void Start()
+    {
+        rootpos = gameObject.transform.position;
+    }
+
+    void Update()
+    {
+        float curspeed = speed * Time.deltaTime;
+
+        if (Input.GetKey("d"))
+        {
+            this.gameObject.transform.position += new Vector3( curspeed, 0.0f, 0.0f);
+        }
+        if (Input.GetKey("s"))
+        {
+            this.gameObject.transform.position -= new Vector3(0.0f, 0.0f, curspeed);
+        }
+        if (Input.GetKey("a"))
+        {
+            this.gameObject.transform.position -= new Vector3(curspeed, 0.0f, 0.0f);
+        }
+        if (Input.GetKey("w"))
+        {
+            this.gameObject.transform.position += new Vector3(0.0f, 0.0f, curspeed);
+        }
+
+        if (Input.GetKey("k"))
+        {
+            Vector3 ex = this.gameObject.transform.position;
+            Wrapper.SavePosition(ex.x, ex.y, ex.z);
+        }
+        if (Input.GetKey("l"))
+        {
+            this.gameObject.transform.position = Wrapper.LoadPosition();
+        }
+        if (Input.GetKey("r"))
+        {
+            gameObject.transform.position = rootpos;
+        }
+    }
+}
---
+++ GameEngineAssignment/Assets/Script/Wrapper.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+
+public class Wrapper : MonoBehaviour
+{
+    public static int hi = 40;
+    const string DLL_NAME = "PluginWrapper";
+
+
+    [DllImport(DLL_NAME)]
+    private static extern void Load2();
+    [DllImport(DLL_NAME)]
+    private static extern void Save2(float x, float y, float z);
+    [DllImport(DLL_NAME)]
+    private static extern float GetX();
+    [DllImport(DLL_NAME)]
+    private static extern float GetY();
+    [DllImport(DLL_NAME)]
+    private static extern float GetZ();
+
+    public static Vector3 LoadPosition()
+    {
+        Load2();
+
+        return new Vector3(GetX(), GetY(), GetZ());
+    }
+    public static void SavePosition(float x, float y, float z)
+    {
+        Save2(x, y, z);
+        
+    }
+
+
+
+    private void Start()
+    {
+
+        
+    }
+}
